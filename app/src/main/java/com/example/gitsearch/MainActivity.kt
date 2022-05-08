964cb2372b46f09c1e1ae0afb69f4f7107df76ce
JakubHerr <kuba2c@seznam.cz>
2022-05-08 14:46:31 +0200

    Add simple search screen

--- app/src/main/java/com/example/gitsearch/MainActivity.kt
+++ app/src/main/java/com/example/gitsearch/MainActivity.kt
@@ -3,13 +3,13 @@ package com.example.gitsearch
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.gitsearch.ui.theme.GitSearchTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,27 +17,58 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             GitSearchTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
+                SearchScreen()
+            }
+        }
+    }
+}
+
+@Preview
+@Composable
+fun SearchScreen() {
+    val scaffoldState = rememberScaffoldState()
+    var textFieldState by remember {
+        mutableStateOf("")
+    }
+
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        scaffoldState = scaffoldState
+    ) {
+        Column(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(32.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            TextField(value = textFieldState, onValueChange = {
+                textFieldState = it
+            }, label = {
+                Text("Enter a username to search")
+            },
+                singleLine = true,
+                modifier = Modifier.fillMaxWidth()
+            )
+            Spacer(modifier = Modifier.padding(16.dp))
+            Button(onClick = {/*TODO add onClick action*/ }) {
+                Text("Submit")
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun UserDetail() {
+
 }
 
-@Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    GitSearchTheme {
-        Greeting("Android")
-    }
+fun RepoDetail() {
+
+}
+
+@Composable
+fun AboutScreen() {
+
 }
